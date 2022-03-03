a29fbde625a31c1be15d820b2b5542b6ae149a34
Song-seung-ho <thdtmdgh2222@gmail.com>
2022-03-03 17:36:56 +0900

    녹음기

---
+++ part2_Chapter7/app/src/main/java/com/example/hanbit/part2_chapter7/MainActivity.kt
@@ -0,0 +1,151 @@
+package com.example.hanbit.part2_chapter7
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hanbit.part2_chapter7.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+    private var state = State.BEFORE_RECORDING
+        set(value) {
+            field = value
+            binding.resetButton.isEnabled = (value == State.AFTER_RECORDING) ||
+                    (value == State.ON_PLAYING )
+            binding.recordButton.updateIconWithState(value)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        requestAudioPermission()
+        initViews()
+        bindViews()
+        initVariables()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initViews() {
+        binding.recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        binding.soundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
+        binding.resetButton.setOnClickListener {
+            stopPlaying()
+            binding.soundVisualizerView.clearVisualization()
+            binding.recordTimeTextView.clearCountTime()
+            state = State.BEFORE_RECORDING
+        }
+        binding.recordButton.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+    }
+
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder()
+            .apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+
+            prepare()
+        }
+        recorder?.start()
+        binding.soundVisualizerView.startVisualizing(false)
+        binding.recordTimeTextView.startCountUp()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        binding.soundVisualizerView.stopVisualizing()
+        binding.recordTimeTextView.stopCountUp()
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                binding.recordTimeTextView.startCountUp()
+                prepare()
+            }
+        player?.setOnCompletionListener {
+            stopPlaying()
+            state = State.AFTER_RECORDING
+        }
+        player?.start()
+        binding.soundVisualizerView.startVisualizing(true)
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        binding.soundVisualizerView.stopVisualizing()
+        state = State.BEFORE_RECORDING
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
+}
---
+++ part2_Chapter7/app/src/main/java/com/example/hanbit/part2_chapter7/State.kt
@@ -0,0 +1,8 @@
+package com.example.hanbit.part2_chapter7
+
+enum class State {
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
