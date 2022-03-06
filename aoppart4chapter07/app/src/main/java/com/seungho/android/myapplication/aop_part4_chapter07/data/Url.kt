3b6c7de7f60fa55d594d60340d9e952a0c22d972
Song-seung-ho <thdtmdgh2222@gmail.com>
2022-03-06 23:17:25 +0900

    저작권 무료 이미지 검색기

---
+++ aoppart4chapter07/app/src/main/java/com/seungho/android/myapplication/aop_part4_chapter07/data/Url.kt
@@ -0,0 +1,5 @@
+package com.seungho.android.myapplication.aop_part4_chapter07.data
+
+object Url {
+    const val UNSPLASH_BASE_URL = "https://api.unsplash.com/"
+}
---
+++ aoppart4chapter07/app/src/main/java/com/seungho/android/myapplication/aop_part4_chapter07/data/models/Position.kt
@@ -0,0 +1,11 @@
+package com.seungho.android.myapplication.aop_part4_chapter07.data.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Position(
+    @SerializedName("latitude")
+    val latitude: Double?,
+    @SerializedName("longitude")
+    val longitude: Double?
+)
